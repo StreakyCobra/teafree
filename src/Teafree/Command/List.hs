49e1e2c2b159a3f068b9b368f111ec5564f137a2
Fabien Dubosson <fabien.dubosson@gmail.com>
2013-11-11 22:58:12 +0100

    Add `prepare` command and some refactoring

---
+++ src/Teafree/Command/List.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+{-
+
+    teafree, a Haskell utility for tea addicts
+    Copyright (C) 2013 Fabien Dubosson <fabien.dubosson@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-}
+
+module Teafree.Command.List
+    ( printList
+    ) where
+
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as TIO
+
+{- Print the list of teas -}
+printList :: IO ()
+printList = do
+    TIO.putStrLn "White\nGreen\nBlack"
